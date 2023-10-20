6ab0fa72b0547804c71c55f1bbef84ac63959974
jay <p0425@nate.com>
2023-10-20 16:41:10 +0900

    주석추가

--- server/config/key.js
+++ server/config/key.js
@@ -1,3 +1,9 @@
+/**
+ * 작성자 : jay
+ * 일자   : 2023.10.19
+ * 설명   : 개발환경, 배포환경 구분
+ */
+
 if(process.env.NODE_ENV === 'production') {
     //process.env.NODE_ENV = 개발/배포 환경 확인
     module.exports = require('./prod');
--- server/config/prod.js
+++ server/config/prod.js
@@ -1,3 +1,9 @@
+/**
+ * 작성자 : jay
+ * 일자   : 2023.10.19
+ * 설명   : 몽고 DB Connection
+ */
+
 module.exports = {
     mongoURI: process.env.MONGO_URI
 }
